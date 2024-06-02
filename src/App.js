6d60f0019f089e9fafffe3ee1a1cdc2780805314
medamin1234 <144592924+medamin1234@users.noreply.github.com>
2024-06-02 15:28:22 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]); // where to store the returned data
+  const [error, setError] = useState(null); // where to store the coming errors
+  const [submittedValue, setSubmittedValue] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    const input = event.target.elements.inputField; // Access the input field directly from the event
+    setSubmittedValue(input.value); // Store the input value in submittedValue
+    console.log(submittedValue);
+  };
+  useEffect(() => {
+    axios
+      .get(
+        `https://jsonplaceholder.typicode.com/posts${
+          submittedValue ? `?userId=${submittedValue}` : ""
+        }`
+      )
+      .then((response) => {
+        setData(response.data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the posts!", error);
+      });
+  }, [submittedValue]);
+  <form onSubmit={handleSubmit}>
+    <input type="text" name="inputField" placeholder="Enter something" />
+    <button type="submit">Submit</button>
+  </form>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="inputField" placeholder="Enter something" />
+        <button type="submit">Submit</button>
+      </form>
+      ;
+      {data.map((v) => (
+        <>
+          <h3>id={v.id}</h3>
+          <h3>userId={v.userId}</h3>
+          <h3>title={v.title}</h3>
+          <p>{v.body}</p>
+        </>
+      ))}
     </div>
   );
-}
-
+};
 export default App;
